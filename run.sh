074e00e50adff5f4e1f2f232ff88ec97082ef1b0
Sheffui <jasiellie79@gmail.com>
2025-02-20 13:44:12 +0800

    added linux run script

---
+++ run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+VENV_DIR=".venv"
+
+# Check if the virtual environment exists
+if [ ! -d "$VENV_DIR" ]; then
+  echo "Virtual environment not found. Creating..."
+  python3 -m venv "$VENV_DIR"
+fi
+
+# Activate the virtual environment
+source "$VENV_DIR/bin/activate"
+
+# Check if requirements.txt exists
+if [ -f "requirements.txt" ]; then
+  echo "Installing requirements..."
+  pip install -r requirements.txt
+  echo "Running main.py"
+  python main.py
+  deactivate
+else
+  echo "requirements.txt not found. Aborting."
+fi
